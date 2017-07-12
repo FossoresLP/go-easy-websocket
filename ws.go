8d9886e4b3bf6a243c5e6d5bdf0b83b14af12c13
Pascal Vorwerk <info@fossores.de>
2017-07-12 12:44:21 +0200

    Implementing authentication using JWT over HTTP basic auth
    Implementing authentication to verify the users connecting using JSON Web Tokens signed with ed25519. The data is submitted using HTTP basic auth with the userid (jwt->sub) as username and the JWT as password as the JS API for WebSocket doesn't allow for custom headers.

--- ws.go
+++ ws.go
@@ -1,8 +1,10 @@
 package websocket
 
 import (
+	"fmt"
 	"net/http"
 
+	jwt "github.com/fossoreslp/go-jwt-ed25519"
 	uuid "github.com/fossoreslp/go.uuid"
 	ws "github.com/gorilla/websocket"
 	"github.com/julienschmidt/httprouter"
@@ -12,10 +14,32 @@ var upgrader = ws.Upgrader{}
 
 // UpgradeHandler upgrades http requests to ws and starts a goroutine for handling ws messages
 func (h *Handler) UpgradeHandler(w http.ResponseWriter, r *http.Request, params ...httprouter.Params) {
+	user, token, ok := r.BasicAuth()
+	fmt.Println(ok)
+	userid, err := uuid.FromString(user)
 	if err != nil {
-		w.WriteHeader(500)
+		w.WriteHeader(403)
 		return
 	}
+	auth, err := jwt.Decode(token)
+	if err != nil {
+		w.WriteHeader(403)
+		return
+	}
+	if !auth.Valid {
+		w.WriteHeader(403)
+		return
+	}
+	subject, err := uuid.FromString(auth.Content.Sub)
+	if err != nil {
+		w.WriteHeader(403)
+		return
+	}
+	if subject != userid {
+		w.WriteHeader(403)
+		return
+	}
+
 	sessionid := uuid.NewV4()
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
