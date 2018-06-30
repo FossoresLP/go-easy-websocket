0f43bcab46bfc0311f0b15324c53ca1dc4e8dbb5
Pascal Vorwerk <info@fossores.de>
2018-06-30 19:16:31 +0200

    Fix unregisterAsListener not handling items at beginning or end of list Fixing a crash that occurred when trying to remove the only, first or last ID in the list. Also preventing the addition of duplicates in almost all cases.

--- listener.go
+++ listener.go
@@ -13,7 +13,7 @@ func (h *Handler) RegisterListenChannel(name string) error {
 	}
 	h.channels[name] = &channel{
 		make(chan *Message, 8),
-		make([]uuid.UUID, 1),
+		make([]uuid.UUID, 0),
 	}
 	go h.channelRoutine(name)
 	return nil
@@ -21,18 +21,40 @@ func (h *Handler) RegisterListenChannel(name string) error {
 
 func (h *Handler) registerAsListener(id uuid.UUID, name string) error {
 	if c, ok := h.channels[name]; ok {
+		for _, lid := range c.listeners {
+			if id == lid {
+				return errors.New("already listening")
+			}
+		}
 		c.listeners = append(c.listeners, id)
 		return nil
 	}
 	return errors.New("channel does not exist")
 }
 
-func (h *Handler) unregisterAsListener(rmid uuid.UUID, name string) {
-	for i, id := range h.channels[name].listeners {
-		if id == rmid {
-			h.channels[name].listeners = append(h.channels[name].listeners[:i], h.channels[name].listeners[i+1:]...)
+func (h *Handler) unregisterAsListener(rmid uuid.UUID, name string) error {
+	if c, ok := h.channels[name]; ok {
+		for i, id := range c.listeners {
+			if id == rmid {
+				if len(c.listeners) <= 1 {
+					c.listeners = []uuid.UUID{}
+					break
+				}
+				if i == 0 {
+					c.listeners = c.listeners[1:]
+					break
+				}
+				if i == len(c.listeners)-1 {
+					c.listeners = c.listeners[:i]
+					break
+				}
+				c.listeners = append(c.listeners[:i], c.listeners[i+1:]...)
+				break
+			}
 		}
+		return nil
 	}
+	return errors.New("channel does not exist")
 }
 
 func (h *Handler) unregisterListener(rmid uuid.UUID) {
